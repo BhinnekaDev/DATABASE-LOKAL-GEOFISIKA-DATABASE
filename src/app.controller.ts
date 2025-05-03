2e7f3dd7d34b814efb5ea7dab9581048586e8f89
fifovalle <fifanaufal10@gmail.com>
2025-05-04 06:27:37 +0700

    feat: menambahkan tampilan api end point lebih menarik

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { AppService } from '@/app.service';
-import { Controller, Get } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
+import { Controller, Get, Header } from '@nestjs/common';
 
 @ApiExcludeController()
 @Controller()
@@ -8,6 +8,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Header('Content-Type', 'text/html')
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,40 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('API END POINT')
-    .setDescription('API LOKAL DATABASE GEOFISIKA')
+    .setDescription('Dokumentasi API Lokal Database Geofisika')
     .setVersion('1.0')
     .addTag('Autentikasi')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: '⚡ Dokumentasi API Geofisika',
+    customCss: `
+        .swagger-ui .topbar {
+            display: none;
+        }
+
+        ::-webkit-scrollbar {
+            width: 10px;
+        }
+
+        ::-webkit-scrollbar-track {
+            background:rgb(255, 255, 255);
+            border-radius: 8px;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            background: linear-gradient(180deg, #1abc9c, #00bcd4);
+            border-radius: 8px;
+        }
+
+        ::-webkit-scrollbar-thumb:hover {
+            background: linear-gradient(180deg, #16a085, #0097a7);
+        }
+        `,
+    customfavIcon: 'https://img.icons8.com/fluency/48/api.png',
+  });
 
   await app.listen(3000);
 }
